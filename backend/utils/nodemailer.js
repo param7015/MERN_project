59d809cf6427068faf7dc64df98a2558e7e51d1f
Paramjeet Singh <paramjeet.sde@gmail.com>
2026-07-11 00:43:46 +0530

    Add port and secure options to nodemailer transport

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -2,6 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
